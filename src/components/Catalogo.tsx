d46704564f8aa3745bb54c10c41eca7de616bec4
Sandro <sandrowonmer@gmail.com>
2025-10-27 21:16:45 -0500

    feat: titulos arreglados

--- src/components/Catalogo.tsx
+++ src/components/Catalogo.tsx
@@ -1,14 +1,14 @@
 import CarouselPro from "@/components/Carousel";
 
 const fotos = [
-  { src: "/assets/foto-n1.png", caption: "Payasito infantil" },
-  { src: "/assets/foto-n2.png", caption: "Payasito infantil" },
-  { src: "/assets/foto-n3.png", caption: "Payasito infantil" },
-  { src: "/assets/foto-n4.png", caption: "Payasito infantil" },
-  { src: "/assets/foto-n5.png", caption: "Payasito infantil" },
-  { src: "/assets/foto-n6.png", caption: "Payasito infantil" },
-  { src: "/assets/foto-n7.png", caption: "Payasito infantil" },
-  { src: "/assets/foto-n8.png", caption: "Payasito infantil" },
+  { src: "/assets/foto-n1.png", caption: "Juego del Calamar" },
+  { src: "/assets/foto-n2.png", caption: "Play boy" },
+  { src: "/assets/foto-n3.1.png", caption: "Gata con botas" },
+  { src: "/assets/foto-n4.png", caption: "Payasita" },
+  { src: "/assets/foto-n5.png", caption: "Caperusita roja" },
+  { src: "/assets/foto-n6.png", caption: "Mario Bross" },
+  { src: "/assets/foto-n7.png", caption: "La monja" },
+  { src: "/assets/foto-n8.png", caption: "Pirata" },
 ];
 
 export default function Galeria() {

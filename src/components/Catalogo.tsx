e24ec02c094aaec265cf2139e4ef863c470ce856
Sandro <sandrowonmer@gmail.com>
2025-10-27 21:28:47 -0500

    feat: img gata con botas tuneada

--- src/components/Catalogo.tsx
+++ src/components/Catalogo.tsx
@@ -3,7 +3,7 @@ import CarouselPro from "@/components/Carousel";
 const fotos = [
   { src: "/assets/foto-n1.png", caption: "Juego del Calamar" },
   { src: "/assets/foto-n2.png", caption: "Play boy" },
-  { src: "/assets/foto-n3.1.png", caption: "Gata con botas" },
+  { src: "/assets/foto-n3.2.png", caption: "Gata con botas" },
   { src: "/assets/foto-n4.png", caption: "Payasita" },
   { src: "/assets/foto-n5.png", caption: "Caperusita roja" },
   { src: "/assets/foto-n6.png", caption: "Mario Bross" },
